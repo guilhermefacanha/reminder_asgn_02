d28b20ccfe9edc7e7449269ee48b06f2d706b1e7
Guilherme Lima Facanha <limafacanhag@student.douglascollege.ca>
2019-01-27 19:44:59 -0800

    Hello World Application Original Commit

---
+++ src/com/csis/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.csis.hello;
+
+
+/**
+ * Hello World Application
+ * Reminder Group
+ * - Guilherme Facanha #300294067
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
